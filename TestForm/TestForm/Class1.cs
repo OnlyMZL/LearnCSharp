e7cda40f358b8a6ade3faa62b838084b526bfae5
OnlyMZL <onlymzl@outlook.com>
2016-10-18 22:10:17 +0800

    form

---
+++ TestForm/TestForm/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command:IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet element)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            GetElemType elem = new GetElemType(doc);
+            Form1 form = new Form1(elem);
+            form.Show();
+            
+
+            return Result.Succeeded;
+        }
+    }
+
+    public class GetElemType
+    {
+        private int elemId;
+        public int ElemId
+        {
+            get { return elemId; }
+            set { elemId = value; }
+        }
+
+        private Element elem;
+        public Element Elem
+        {
+            get { return elem; }
+            set { elem = value; }
+        }
+
+        public GetElemType(Document doc)
+        {
+            ElementId elemid = new ElementId(elemId);
+            elem=doc.GetElement(elemid);
+        }
+
+    }
+}
---
+++ TestForm/TestForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1(GetElemType data)
+        {
+            elemDate = data;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            elemDate.ElemId = int.Parse(textBox1.Text);
+            MessageBox.Show(elemDate.Elem.Category. ToString());
+            
+        }
+    }
+}
