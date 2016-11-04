4844347ba8589acc36e2ff6ca9c478253784b1c0
OnlyMZL <onlymzl@outlook.com>
2016-11-04 22:44:53 +0800

    02

--- C#Primer/基础复习/005/06 乘法口诀/Program.cs
+++ C#Primer/基础复习/005/06 乘法口诀/Program.cs
@@ -12,6 +12,61 @@ namespace _06_乘法口诀
         {
 
 
+            #region 下三角乘法口诀表
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("{0}X{1}={2}\t", j, i, i * j);
+            //    }
+            //    Console.Write("\n");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+
+            //Console.WriteLine("请输入一个整数");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i <= num; i++)
+            //{
+            //    int j = num - i;
+            //    Console.WriteLine("{0}+{1}={2}", i, j, num);
+            //}
+            //Console.ReadLine();
+
+
+            int sum = 0;
+            bool b = true;
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine("请输入第{0}个人的年龄",i+1);
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age > 0 && age < +100)
+                    {
+                        sum += age;
+                    }
+                    else
+                    {
+                        Console.WriteLine("输入的年龄有误，程序将退出！！");
+                        b = false;
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("输入有误，程序将退出！！");
+                    b = false;
+                    break;
+                }
+            }
+            if (b)
+            {
+                Console.WriteLine("这五个人的平均年龄是{0}岁", sum / 5);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ C#Primer/基础复习/005/13 枚举/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_枚举
+{
+    //声明枚举类型Sesons
+    enum Sesons
+    {
+        春,
+        夏,
+        秋,
+        冬
+    }
+    //声明枚举类型QQState
+    enum QQState
+    {
+        OnLine,
+        OffLine,
+        Leave,
+        Busy,
+        QMe
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用枚举类型
+            Sesons s = Sesons.夏;
+            QQState state = QQState.QMe;
+
+        }
+    }
+}
