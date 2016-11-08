80752a1fd74ea9d83a22656e1a68414b6a61a88e
OnlyMZL <onlymzl@outlook.com>
2016-11-08 12:28:04 +0800

    08

--- C#Primer/基础复习/008/01 方法的练习/Program.cs
+++ C#Primer/基础复习/008/01 方法的练习/Program.cs
@@ -37,13 +37,170 @@ namespace _01_方法的练习
              * 81、写一个方法，用来判断用户输入的数字是不是质数 
              *     再写一个方法 要求用户只能输入数字 输入有误就一直让用户输入数字
              * **********************************/
-            Console.WriteLine("请输入一个整数");
-            bool b = IsPrimeNumber(Console.ReadLine());
-            Console.WriteLine(b);
-            Console.ReadKey();
+            //Console.WriteLine("请输入一个整数");
+            //bool b = IsPrimeNumber(Console.ReadLine());
+            //Console.WriteLine(b);
+            //Console.ReadKey();
+
+
+
+            /***************************95******************************
+             * 95、接受输入后判断其等级并显示出来。
+             * 判断依据如下：等级 ={ 优 （90~100分）；良 （80~89分）；中 （60~69分）；差 （0~59分）；}
+             * *******************************************************/
+
+            //Console.WriteLine("请输入成绩");
+            //double num = GetDoubleNum(Console.ReadLine());
+            //JudgeLevel(num);
+            //Console.ReadLine();
+
+
+
+            /*****************************97***********************************
+             * 97、请将字符串数组{ "中国", "美国", "巴西", "澳大利亚", "加拿大" }中的内容反转
+             * ***********************************************************************/
+
+            //string[] contries = { "中国", "美国", "巴西", "澳大利亚", "加拿大" };
+            //Console.WriteLine("原数组内容：");
+            //for (int i = 0; i < contries.Length; i++)
+            //{
+            //    Console.Write("\t" + contries[i]);
+            //}
+            //Console.WriteLine("\n翻转数组内容：");
+            //Reverse(contries);  //调用方法翻转数组
+            //for (int i = 0; i < contries.Length; i++)
+            //{
+            //    Console.Write("\t" + contries[i]);
+            //}
+            //Console.ReadLine();
+
+
+
+            /*****************************98********************************
+             * 98写一个方法 计算圆的面积和周长  面积是 pI*R*R  周长是 2*Pi*r
+             * ***************************************************************/
+
+            //Console.WriteLine("请输入圆的半径");
+            //double r=GetDoubleNum(Console.ReadLine());
+            //CycleArandLen(r);
+            //Console.ReadLine();
+
+
+
+            /****************************100***************************************
+             * 100、计算任意多个数间的最大值（提示：params）。 
+             * ****************************************************/
+
+
+
+            /****************************101***************************************
+             * 101、请通过冒泡排序法对整数数组{ 1, 3, 5, 7, 90, 2, 4, 6, 8, 10 }实现升序排序。
+             * *****************************************************************/
+            int[] nums = { 1, 3, 5, 7, 90, 2, 4, 6, 8,10};
+            Console.WriteLine("原数组内容");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            BubbleSort(nums);
+            Console.WriteLine("升序排列后的数组内容");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+
+
         }
 
 
+        /// <summary>
+        /// 冒泡排序（升序）
+        /// </summary>
+        /// <param name="nums">待排序的数组</param>
+        public static void BubbleSort(int[] nums)
+        {
+            int temp = 0;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for (int j = 0; j < nums.Length-1-i; j++)
+                {
+                    if (nums[j] > nums[j+1])
+                    {
+                        temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 计算圆面积和周长并输出
+        /// </summary>
+        /// <param name="r">圆半径</param>
+        public static void CycleArandLen(double r)
+        {
+            double ar = Math.PI * r * r;
+            double len = Math.PI * r * 2;
+            Console.WriteLine("圆面积是：{0}\n圆周长是：{1}", ar, len);
+        }
+
+
+        /// <summary>
+        /// 翻转数组
+        /// </summary>
+        /// <param name="s">要反转的数组</param>
+        public static void Reverse(string[] s)
+        {
+            string temp="";
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = temp;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 根据分数判断等级，并输出
+        /// </summary>
+        /// <param name="num">分数</param>
+        public static void JudgeLevel(double num)
+        {
+            while (true)
+            {
+                if (num >= 90 && num <= 100)
+                {
+                    Console.WriteLine("优");
+                    break;
+                }
+                else if (num >= 80 && num < 90)
+                {
+                    Console.WriteLine("良");
+                    break;
+                }
+                else if (num >= 60 && num < 80)
+                {
+                    Console.WriteLine("中");
+                    break;
+                }
+                else if (num >= 0 && num < 60)
+                {
+                    Console.WriteLine("差");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("输入有误，请重新输入");
+                    num = GetDoubleNum(Console.ReadLine());
+                }
+            }
+        }
 
 
         /// <summary>
@@ -66,6 +223,28 @@ namespace _01_方法的练习
         }
 
 
+        /// <summary>
+        /// 将用户输入转换为double类型数字
+        /// </summary>
+        /// <param name="s">用户输入的内容</param>
+        /// <returns>返回double类型数字</returns>
+        public static double GetDoubleNum(string s)
+        {
+            double num = 0;
+            while (true)
+            {
+                try
+                {
+                    num = Convert.ToDouble(s);
+                    return num;
+                }
+                catch
+                {
+                    Console.WriteLine("输入有误，请重新输入");
+                    s = Console.ReadLine();
+                }
+            }
+        }
 
         /// <summary>
         /// 将用户输入的内容转换为整数
@@ -74,11 +253,12 @@ namespace _01_方法的练习
         /// <returns>返回整数</returns>
         public static int GetIntNum(string s)
         {
+            int num = 0;
             while (true)
             {
                 try
                 {
-                    int num = Convert.ToInt32(s);
+                    num = Convert.ToInt32(s);
                     return num;
                 }
                 catch
