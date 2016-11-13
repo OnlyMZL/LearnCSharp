552a8f9233562a57d8507df3f4550fd0235f7209
OnlyMZL <onlymzl@outlook.com>
2016-11-13 22:33:27 +0800

    a

---
+++ C#Primer/基础复习/009/10 继承练习/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_继承练习
+{
+    class Person
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                _age = value;
+            }
+        }
+        private char _gender;
+        public char Gender
+        {
+            get
+            {
+                return _gender;
+            }
+
+            set
+            {
+                _gender = value;
+            }
+        }
+
+        
+    }
+}
---
+++ C#Primer/基础复习/009/10 继承练习/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_继承练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //记者：我是记者  我的爱好是新闻  我的年龄是25岁 我是一个男记者
+            //司机：我叫奔驰  我的年龄是30 我的驾龄是5年  我是男的
+            //学生：我叫111  我的年龄是18 我是男生  我还有3年毕业
+        }
+    }
+}
