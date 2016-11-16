a5b3cd95d8b270d66f150e8489e76aab39b1119b
OnlyMZL <onlymzl@outlook.com>
2016-11-16 21:32:45 +0800

    publish

---
+++ C#Primer/基础复习/010/02 File类/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_File类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //在指定路径创建文件
+            //File.Create(@"E:\C#\new.txt");
+
+            //向文件中写入
+            //string str = "1234567890";
+            //byte[] buffer = Encoding.Default.GetBytes(str);
+            //File.WriteAllBytes(@"E:\C#\new.txt", buffer);
+
+            //从文件中读取数据
+            //byte[] buffer = File.ReadAllBytes(@"E:\C#\new.txt");
+            //string s = Encoding.Default.GetString(buffer);
+            //Console.WriteLine(s);
+
+            //向文件中追加写入内容
+            File.AppendAllText(@"E:\C#\new.txt","12345678");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ C#Primer/基础复习/010/03 文件流/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _03_文件流
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1、读取文件内容
+            ////创建FileStream类对象
+            //FileStream stRead = new FileStream(@"E:\C#\st.txt",FileMode.OpenOrCreate,FileAccess.Read);
+            ////声明一个5M 的字节数组
+            //byte[] buffer = new byte[1024 * 1024 * 5];
+            ////声明变量存储实际读取到的字节数
+            //int r= stRead.Read(buffer, 0, buffer.Length);
+            ////将指定字节数组的一个字节序列转换成字符串
+            //string str = Encoding.Default.GetString(buffer,0,r);
+            //Console.WriteLine(str);
+            ////关闭流
+            //stRead.Close();
+            ////释放内存
+            //stRead.Dispose();
+            //Console.ReadKey();
+
+
+            //2、写入文件数据
+            //创建FileStream类对象
+            //using (FileStream stWrite = new FileStream(@"E:\C#\st.txt", FileMode.OpenOrCreate, FileAccess.Write))
+            //{
+            //    string str = "33333333";
+            //    byte[] buffer = Encoding.Default.GetBytes(str);
+            //    stWrite.Write(buffer, 0, buffer.Length);
+            //}
+            //Console.WriteLine("写入成功");
+            //Console.ReadKey();
+
+
+            //3、通过FileStream类复制多媒体文件
+            string source = @"E:\C#\1.avi";
+            string target = @"E:\C#\2.avi";
+            CopyTo(source,target);
+            Console.WriteLine("写入成功");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 复制source数据到target
+        /// </summary>
+        /// <param name="source">数据源路径</param>
+        /// <param name="target">目标路径</param>
+        public static void CopyTo(string source, string target)
+        {
+            using (FileStream fsRead = new FileStream(source, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                using (FileStream fsWrite = new FileStream(target, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = 1;
+                    while (true)
+                    {
+                        r = fsRead.Read(buffer, 0, buffer.Length);
+                        if (r == 0)
+                            break;
+                        fsWrite.Write(buffer, 0, r);
+                    }
+                }
+            }
+        }
+        
+    }
+}
