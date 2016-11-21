e476504acca29a6c200c3f19cb49cab960fc7ae9
OnlyMZL <onlymzl@outlook.com>
2016-11-21 22:07:28 +0800

    winform

---
+++ C#Primer/基础复习/013/09 ListBox控件/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _09_ListBox控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstb = new System.Windows.Forms.ListBox();
+            this.picb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstb
+            // 
+            this.lstb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstb.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lstb.FormattingEnabled = true;
+            this.lstb.ItemHeight = 12;
+            this.lstb.Location = new System.Drawing.Point(32, 29);
+            this.lstb.Name = "lstb";
+            this.lstb.Size = new System.Drawing.Size(147, 520);
+            this.lstb.TabIndex = 0;
+            this.lstb.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstb_MouseDoubleClick);
+            // 
+            // picb
+            // 
+            this.picb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picb.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.picb.Location = new System.Drawing.Point(200, 29);
+            this.picb.Name = "picb";
+            this.picb.Size = new System.Drawing.Size(695, 520);
+            this.picb.TabIndex = 1;
+            this.picb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 576);
+            this.Controls.Add(this.picb);
+            this.Controls.Add(this.lstb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstb;
+        private System.Windows.Forms.PictureBox picb;
+    }
+}
+
---
+++ C#Primer/基础复习/013/09 ListBox控件/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09_ListBox控件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] path = Directory.GetFiles(@"E:\C#\Picture", "*.jpg");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picb.Image =Image.FromFile(path[0]);
+            picb.SizeMode = PictureBoxSizeMode.Zoom;
+            foreach (var item in path)
+            {
+                lstb.Items.Add(Path.GetFileName(item));
+            }
+        }
+
+        private void lstb_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            picb.Image = Image.FromFile(path[lstb.SelectedIndex]);
+        }
+    }
+}
