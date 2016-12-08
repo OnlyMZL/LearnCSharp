c2c4d8dc32350acc22927fcbe262b527ca211403
OnlyMZL <onlymzl@outlook.com>
2016-12-08 22:15:20 +0800

    ado.net

--- C#Primer/基础复习/015/01 ADO.NET连接/Form1.Designer.cs
+++ C#Primer/基础复习/015/01 ADO.NET连接/Form1.Designer.cs
@@ -28,21 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInsert = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnId = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtInsert
+            // 
+            this.txtInsert.Location = new System.Drawing.Point(56, 44);
+            this.txtInsert.Name = "txtInsert";
+            this.txtInsert.Size = new System.Drawing.Size(100, 21);
+            this.txtInsert.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(69, 85);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "插入数据";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(198, 43);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 21);
+            this.txtId.TabIndex = 2;
+            // 
+            // btnId
+            // 
+            this.btnId.Location = new System.Drawing.Point(183, 84);
+            this.btnId.Name = "btnId";
+            this.btnId.Size = new System.Drawing.Size(130, 23);
+            this.btnId.TabIndex = 3;
+            this.btnId.Text = "通过编号查询部门";
+            this.btnId.UseVisualStyleBackColor = true;
+            this.btnId.Click += new System.EventHandler(this.btnId_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(638, 401);
+            this.Controls.Add(this.btnId);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtInsert);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInsert;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnId;
     }
 }
 
--- C#Primer/基础复习/015/01 ADO.NET连接/Form1.cs
+++ C#Primer/基础复习/015/01 ADO.NET连接/Form1.cs
@@ -11,40 +11,121 @@ using System.Data.SqlClient;
 
 namespace _01_ADO.NET连接
 {
+
+    public enum SqlCommandFunction  //声明枚举，判断要执行的SqlCommand的方法
+    {
+        ExecuteNonQuery,
+        ExecuteScalar,
+        ExecuteReader
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            ////连接数据库
+            //string constr = "server=.;user=sa;pwd=sa;database=mydb001";
+            //SqlConnection conn = new SqlConnection(constr);
+            //using (conn)
+            //{
+            //    conn.Open();
+            //    MessageBox.Show("数据库连接成功", "提示信息");
+
+            //    #region SqlCommand使用
+            //    ////插入数据  ExecuteNonQuery方法
+            //    //SqlCommand cmd = new SqlCommand("insert into mytable001(name) values('111')", conn);
+            //    //int i = cmd.ExecuteNonQuery();
+            //    //MessageBox.Show(i + "行受影响", "提示信息");
+            //    ////删除数据
+            //    //SqlCommand cmd2 = new SqlCommand("delete from mytable001 where name='111'", conn);
+            //    //int j = cmd2.ExecuteNonQuery();
+            //    //MessageBox.Show(j + "行受影响", "提示信息");
+            //    #endregion
+
+            //    conn.Close();
+            //    MessageBox.Show("数据库关闭成功", "提示");
+            //    conn.Dispose();
+
+            //}
+        }
+
+        /// <summary>
+        /// 插入数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
             //连接数据库
+            SqlConnection conn = GetSqlConnection();
+            //执行数据库命令
+            GetSqlCommand("insert into mytable001(name) values('" + txtInsert.Text + "')", conn,SqlCommandFunction.ExecuteNonQuery);
+            GetSqlCommand("delete from mytable001 where age<18", conn,SqlCommandFunction.ExecuteNonQuery);
+            //关闭数据库并释放资源
+            CloseSqlConnection(conn);
+        }
+        
+        private void btnId_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = GetSqlConnection();
+            string str = (string)GetSqlCommand("select department from mytable001 where id=" + txtId.Text, conn, SqlCommandFunction.ExecuteScalar);
+            MessageBox.Show(str);
+        }
+
+        
+        #region 封装的数据库方法
+        /// <summary>
+        /// 连接数据库
+        /// </summary>
+        /// <returns>连接到的数据库对象</returns>
+        public static SqlConnection GetSqlConnection()
+        {
             string constr = "server=.;user=sa;pwd=sa;database=mydb001";
             SqlConnection conn = new SqlConnection(constr);
-            using (conn)
-            {
-                conn.Open();
-                MessageBox.Show("数据库连接成功", "提示信息");
-
-                #region SqlCommand使用
-                ////插入数据  ExecuteNonQuery方法
-                //SqlCommand cmd = new SqlCommand("insert into mytable001(name) values('111')", conn);
-                //int i = cmd.ExecuteNonQuery();
-                //MessageBox.Show(i + "行受影响", "提示信息");
-                ////删除数据
-                //SqlCommand cmd2 = new SqlCommand("delete from mytable001 where name='111'", conn);
-                //int j = cmd2.ExecuteNonQuery();
-                //MessageBox.Show(j + "行受影响", "提示信息");
-                #endregion
-
-                conn.Close();
-                MessageBox.Show("数据库关闭成功", "提示");
-                conn.Dispose();
-
-            }
+            conn.Open();
+            return conn;
         }
+        /// <summary>
+        /// 执行数据库命令  SqlCommand
+        /// </summary>
+        /// <param name="str">要执行的命令</param>
+        /// <param name="conn">已经打开的数据库连接</param>
+        /// <param name="fun">cmd要执行的方法</param>
+        /// <returns>cmd执行方法的返回内容</returns>
+        public static object GetSqlCommand(string str, SqlConnection conn, SqlCommandFunction fun)
+        {
+            object obj;
+            SqlCommand cmd = new SqlCommand(str, conn);
+            switch ((int)fun)
+            {
+                case 0: obj = cmd.ExecuteNonQuery(); break;
+                case 1: obj = cmd.ExecuteScalar(); break;
+                case 2: obj = cmd.ExecuteReader(); break;
+                default:
+                    obj = null;
+                    MessageBox.Show("命令执行失败");
+                    break;
+            }
+            return obj;
+        }
+        /// <summary>
+        /// 关闭数据库
+        /// </summary>
+        /// <param name="conn">已经打开的数据库</param>
+        public static void CloseSqlConnection(SqlConnection conn)
+        {
+            conn.Close();
+            conn.Dispose();
+            MessageBox.Show("数据库成功关闭", "提示");
+        }
+
+        #endregion
+
+
     }
 }
