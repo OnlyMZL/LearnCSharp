85b54bf846cd46eed5ffb9351c7927eedc60af0b
OnlyMZL <onlymzl@outlook.com>
2017-06-23 00:22:20 +0800

    汉诺塔

---
+++ 算法练习/001汉诺塔/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _001汉诺塔
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTipper = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTipper
+            // 
+            this.lblTipper.AutoSize = true;
+            this.lblTipper.Location = new System.Drawing.Point(31, 41);
+            this.lblTipper.Name = "lblTipper";
+            this.lblTipper.Size = new System.Drawing.Size(77, 12);
+            this.lblTipper.TabIndex = 1;
+            this.lblTipper.Text = "请输入层数：";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(50, 175);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 12);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblTipper);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTipper;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 算法练习/001汉诺塔/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _001汉诺塔
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblAnswer.Text = null;
+                string input = textBox1.Text;
+                int inputNum = Convert.ToInt32(input);
+                if (inputNum < 1 || inputNum > 64)
+                {
+                    MessageBox.Show("输入有误，请输入1--64整数！", "错误提示");
+                    textBox1.Text = null;
+                    textBox1.Focus();
+                    return;
+                }
+                int j = MoveBlock(inputNum);
+                lblAnswer.Text = "所需移动" + j + "次";
+                i = 0;  //初始化计数器
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("输入有误，请输入1--64整数！", "错误提示");
+            }
+        }
+
+        int i = 0;  //计数器
+        public int MoveBlock(int n)
+        {
+            if (n == 1)
+            {
+                i++;  //只需移动一次  a-->c
+            }
+            else
+            {
+                MoveBlock(n - 1);  //由a--》b
+                i++;
+                MoveBlock(n - 1);  //由b-->c
+            }
+            return i;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+}
